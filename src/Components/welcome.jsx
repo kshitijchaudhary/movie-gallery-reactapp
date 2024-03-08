c1e13cf845d6fde5244655e2e3d28c758ad1e195
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-03-07 21:17:56 -0500

    first commit

---
+++ src/Components/welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieGallery = () => {
+    <div className='container'>
+        <div className='header'>
+            <div className='text'><h1>Movie Gallery</h1></div>
+        </div>
+    </div>
+}
+
+
+export default MovieGallery
